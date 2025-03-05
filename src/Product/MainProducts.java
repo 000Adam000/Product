6fe5ab6a074ef210a31d158e4a7f19764e16c9d2
Adam <tiu.it.20adam.m.r.f04@gmail.com>
2025-03-05 10:19:58 +0300

    Error handling the user input number (ch) and change it's variable to string

--- src/Product/MainProducts.java
+++ src/Product/MainProducts.java
@@ -15,46 +15,72 @@ public class MainProducts {
         
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("choose:\n1- show TV\n2- show PC");
-        int ch = scanner.nextInt();
-        switch(ch){
-            case 1:
-                
-        tv.setID(101);
-        tv.setName("LG");
-        tv.setCount(4);
-        tv.setPrice(500);
-        tv.setPlace("DotNet");
-        tv.setTVtype("4K");
-        tv.setsize(60);
-        tv.setGaranty(2);
-        tv.setcompany("LG Company");
+        int validNumber = 0;
         
-        
-        System.out.println("id\t name\t count\t price\t seller\t TV type\tTV size\t garanty\t company");
-        System.out.println("*****************************************************************************");
-        System.out.println(tv.getID()+"\t "+tv.getName()+"\t "+tv.getCount()+"\t "+tv.getPrice()+"\t "+tv.getplace()+"\t "+tv.getTVtype()+"\t\t "+tv.getsize()+"\t "+tv.getgaranty()+"\t\t "+tv.getcompany() );
-        break;
-        
-            case 2:
-                
-        pc.setID(102);
-        pc.setName("thinkpad");
-        pc.setCount(8);
-        pc.setPrice(500);
-        pc.setPlace("DotNet");
-        pc.setMonitortype("4K");
-        pc.setModel("lenovo");
-        pc.setGaranty(2);
-        pc.setcompany("LENOVO COMPANY");
-        
-        
-        System.out.println("id\t name\t count\t price\t seller\t Monitor type\tTV Model\t garanty\t company");
-        System.out.println("*****************************************************************************");
-        System.out.println(pc.getID()+"\t "+pc.getName()+"\t "+pc.getCount()+"\t "+pc.getPrice()+"\t "+pc.getplace()+"\t "+pc.getMonitortype()+"\t\t "+pc.getModel()+"\t "+pc.getgaranty()+"\t\t "+pc.getcompany() );
-        break;
-        
+        while (true) {
+            System.out.println();
+            System.out.println("Please enter either 1- to show TV or 2- to show PC or 0- Close: ");
+            String ch = scanner.nextLine();
+            
+            try {
+                int number = Integer.parseInt(ch);
+                if (number == 1 || number == 2 || number == 0) {
+                    validNumber = number;
+                    System.out.println("Success! You entered: " + validNumber);
+
+                    switch(ch){
+                        case "1":
+                            
+                    tv.setID(101);
+                    tv.setName("LG");
+                    tv.setCount(4);
+                    tv.setPrice(500);
+                    tv.setPlace("DotNet");
+                    tv.setTVtype("4K");
+                    tv.setsize(60);
+                    tv.setGaranty(2);
+                    tv.setcompany("LG Company");
+                    
+                    
+                    System.out.println("id\t name\t count\t price\t seller\t TV type\tTV size\t garanty\t company");
+                    System.out.println("*****************************************************************************");
+                    System.out.println(tv.getID()+"\t "+tv.getName()+"\t "+tv.getCount()+"\t "+tv.getPrice()+"\t "+tv.getplace()+"\t "+tv.getTVtype()+"\t\t "+tv.getsize()+"\t "+tv.getgaranty()+"\t\t "+tv.getcompany() );
+                    break;
+                    
+                        case "2":
+                            
+                    pc.setID(102);
+                    pc.setName("thinkpad");
+                    pc.setCount(8);
+                    pc.setPrice(500);
+                    pc.setPlace("DotNet");
+                    pc.setMonitortype("4K");
+                    pc.setModel("lenovo");
+                    pc.setGaranty(2);
+                    pc.setcompany("LENOVO COMPANY");
+                    
+                    
+                    System.out.println("id\t name\t count\t price\t seller\t Monitor type\tTV Model\t garanty\t company");
+                    System.out.println("*****************************************************************************");
+                    System.out.println(pc.getID()+"\t "+pc.getName()+"\t "+pc.getCount()+"\t "+pc.getPrice()+"\t "+pc.getplace()+"\t "+pc.getMonitortype()+"\t\t "+pc.getModel()+"\t "+pc.getgaranty()+"\t\t "+pc.getcompany() );
+                    break;
+                    
+                        case "0":
+
+                    System.exit(0);
+
+                    
+                    }
+                    
+                } else {
+                    System.out.println("Error: " + number + " is not 1 or 2. Please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: '" + ch + "' is not a valid number. Please try again.");
+            }
         }
+        
+
         
 
         
